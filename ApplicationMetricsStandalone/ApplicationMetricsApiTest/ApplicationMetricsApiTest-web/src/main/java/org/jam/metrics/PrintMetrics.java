f7e190d8b77c5855126d894b875d9f94951c6f64
panossot <psotirop@redhat.com>
2018-07-25 12:46:22 +0200

    Continuing rest api examples for the standalone version

--- ApplicationMetricsStandalone/ApplicationMetricsApiTest/ApplicationMetricsApiTest-web/src/main/java/org/jam/metrics/PrintMetrics.java
+++ ApplicationMetricsStandalone/ApplicationMetricsApiTest/ApplicationMetricsApiTest-web/src/main/java/org/jam/metrics/PrintMetrics.java
@@ -16,9 +16,14 @@
  */
 package org.jam.metrics;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -32,6 +37,9 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import org.jam.metrics.applicationmetricsapi.MetricsCacheApi;
 import org.jam.metrics.applicationmetricsapi.MetricsPropertiesApi;
+import org.jam.metrics.applicationmetricslibrary.MetricObject;
+import org.jam.metrics.applicationmetricslibrary.MetricsCache;
+import org.jam.metrics.applicationmetricslibrary.MetricsCacheCollection;
 import org.jam.metrics.applicationmetricsproperties.MetricProperties;
 import static org.junit.Assert.fail;
 import static org.junit.Assert.assertTrue;
@@ -57,7 +65,7 @@ public class PrintMetrics extends HttpServlet {
     private String groupName = "myTestGroup";
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, InterruptedException {
         response.setContentType("text/html;charset=UTF-8");
         initializeMetricProperties();
         
@@ -74,29 +82,119 @@ public class PrintMetrics extends HttpServlet {
             out.println("<br>Successful Run ...</br>");
             
             Client client = ClientBuilder.newClient();
-            WebTarget target = client.target("http://localhost:8080").path("/ApplicationMetricsApiTest-web-1.0.5.Final-SNAPSHOT/rest/Metrics/MetricProperties/get/myTestGroup/databaseStore");
+            WebTarget target = client.target("http://localhost:8080").path("/ApplicationMetricsApiTest-web-1.0.5.Final-SNAPSHOT/rest/Metrics/MetricList/myTestGroup/print");
             Invocation.Builder invocationBuilder = target.request(MediaType.APPLICATION_JSON);
             Response responseJaxrs = invocationBuilder.get();
             if (responseJaxrs.getStatus() != 200) {
-                fail("Rest Api call failed...");
+                fail("<br/>Rest Api call failed...");
             } else {
                 String rs = responseJaxrs.readEntity(String.class);
                 if (rs == null) {
-                    fail("Rest Api call failed...");
+                    fail("<br/>Rest Api call failed...");
                 }
-                assertTrue("DatabaseStore should be disabled... ", rs.compareTo("false")==0);
-                out.println("databaseStore : " + rs);
+                out.println("<br/>printMetricsList : " + rs);
+            }
+
+            metricsApiSessionBean.countMethod();
+            target = client.target("http://localhost:8080").path("/ApplicationMetricsApiTest-web-1.0.5.Final-SNAPSHOT/rest/Metrics/MetricList/myTestGroup");
+            invocationBuilder = target.request(MediaType.APPLICATION_JSON);
+            responseJaxrs = invocationBuilder.get();
+            if (responseJaxrs.getStatus() != 200) {
+                fail("<br/>Rest Api call failed...");
+            } else {
+                String rs = responseJaxrs.readEntity(String.class);
+                MetricsCache mc = new ObjectMapper().readValue(rs, MetricsCache.class);
+
+                HashSet<MetricObject> metricsCache = mc.getMetricCache();
+
+                assertTrue("<br/>There should be two metrics inside the cache", metricsCache.size() == 2);
+
+                for (MetricObject mObject : metricsCache) {
+                    assertTrue("There should be two metrics values for each metric inside the cache", mObject.getMetric().size() == 2);
+                    out.println("<br/>" + mObject.getName() + "," + mObject.getMetric().toString());
+                }
+            }
+            
+            target = client.target("http://localhost:8080").path("/ApplicationMetricsApiTest-web-1.0.5.Final-SNAPSHOT/rest/Metrics/MetricProperties/get/myTestGroup/cacheEnabled");
+            invocationBuilder = target.request(MediaType.APPLICATION_JSON);
+            responseJaxrs = invocationBuilder.get();
+            if (responseJaxrs.getStatus() != 200) {
+                fail("<br/>Rest Api call failed...");
+            } else {
+                String rs = responseJaxrs.readEntity(String.class);
+                if (rs == null) {
+                    fail("<br/>Rest Api call failed...");
+                }
+                assertTrue("<br/>The cache storage should be enabled... ", rs.compareTo("true")==0);
+                out.println("<br/>" + rs);
+            }
+            
+            metricsApiSessionBean.countMethod();
+            target = client.target("http://localhost:8080").path("/ApplicationMetricsApiTest-web-1.0.5.Final-SNAPSHOT/rest/Metrics/MetricProperties/set/myTestGroup/cacheEnabled").queryParam("enableCacheStore", "false");
+            invocationBuilder = target.request(MediaType.APPLICATION_JSON);
+            responseJaxrs = invocationBuilder.get();
+            if (responseJaxrs.getStatus() != 200) {
+                out.println(responseJaxrs.getStatus());
+                fail("Rest Api call failed...");
+            } else {
+               HashSet<MetricObject> metricsCache = MetricsCacheCollection.getMetricsCacheCollection().getMetricsCacheInstance(groupName).getMetricCache();
+
+                assertTrue("There should be two metrics inside the cache", metricsCache.size() == 2);
+
+                for (MetricObject mObject : metricsCache) {
+                    assertTrue("There should be three metrics values for each metric inside the cache", mObject.getMetric().size() == 3);
+                    out.println("<br/>" + mObject.getName() + "," + mObject.getMetric().toString());
+                }
+            }
+            
+            target = client.target("http://localhost:8080").path("/ApplicationMetricsApiTest-web-1.0.5.Final-SNAPSHOT/rest/Metrics/MetricProperties/get/myTestGroup/rhqMonitoring");
+            invocationBuilder = target.request(MediaType.APPLICATION_JSON);
+            responseJaxrs = invocationBuilder.get();
+            if (responseJaxrs.getStatus() != 200) {
+                fail("<br/>Rest Api call failed...");
+            } else {
+                String rs = responseJaxrs.readEntity(String.class);
+                if (rs == null) {
+                    fail("<br/>Rest Api call failed...");
+                }
+                assertTrue("<br/>RHQ monitoring should be disabled... ", rs.compareTo("false")==0);
+                out.println("<br/>rhqMonitoring : " + rs);
+            }
+            
+            target = client.target("http://localhost:8080").path("/ApplicationMetricsApiTest-web-1.0.5.Final-SNAPSHOT/rest/Metrics/MetricProperties/set/myTestGroup/rhqMonitoring").queryParam("rhqMonitoringEnabled", "true");
+            invocationBuilder = target.request(MediaType.APPLICATION_JSON);
+            responseJaxrs = invocationBuilder.get();
+            if (responseJaxrs.getStatus() != 200) {
+                fail("<br/>Rest Api call failed...");
+            } else {
+                String rhqMonitoring = MetricsPropertiesApi.getProperties(groupName).getRhqMonitoring();
+                assertTrue("<br/>RHQ monitoring should be enabled... ", rhqMonitoring.compareTo("true")==0);
+                out.println("<br/>rhqMonitoring : " + rhqMonitoring);
+            }
+            
+            target = client.target("http://localhost:8080").path("/ApplicationMetricsApiTest-web-1.0.5.Final-SNAPSHOT/rest/Metrics/MetricProperties/get/myTestGroup/databaseStore");
+            invocationBuilder = target.request(MediaType.APPLICATION_JSON);
+            responseJaxrs = invocationBuilder.get();
+            if (responseJaxrs.getStatus() != 200) {
+                fail("<br/>Rest Api call failed...");
+            } else {
+                String rs = responseJaxrs.readEntity(String.class);
+                if (rs == null) {
+                    fail("<br/>Rest Api call failed...");
+                }
+                assertTrue("<br/>DatabaseStore should be disabled... ", rs.compareTo("false")==0);
+                out.println("<br/>databaseStore : " + rs);
             }
             
             target = client.target("http://localhost:8080").path("/ApplicationMetricsApiTest-web-1.0.5.Final-SNAPSHOT/rest/Metrics/MetricProperties/set/myTestGroup/databaseStore").queryParam("databaseStoreEnabled", "true");
             invocationBuilder = target.request(MediaType.APPLICATION_JSON);
             responseJaxrs = invocationBuilder.get();
             if (responseJaxrs.getStatus() != 200) {
-                fail("Rest Api call failed...");
+                fail("<br/>Rest Api call failed...");
             } else {
                 String databaseStore = MetricsPropertiesApi.getProperties(groupName).getDatabaseStore();
-                assertTrue("DatabaseStore should be enabled... ", databaseStore.compareTo("true")==0);
-                out.println("databaseStore : " + databaseStore);
+                assertTrue("<br/>DatabaseStore should be enabled... ", databaseStore.compareTo("true")==0);
+                out.println("<br/>databaseStore : " + databaseStore);
             }
             
             out.println("</body>");
@@ -128,7 +226,11 @@ public class PrintMetrics extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
     }
 
     /**
@@ -142,7 +244,11 @@ public class PrintMetrics extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
     }
 
     /**
